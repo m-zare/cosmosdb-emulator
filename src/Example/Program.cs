b7561e11fb9337783a1c5286ec390a53d37ede2c
Konstantin Yakushev <Konstantin.Yakushev@wolterskluwer.com>
2024-03-18 15:29:37 +0100

    Create an Example CosmosDB runner

---
+++ src/Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Cosmos;
+
+using CosmosClient client = new(
+    accountEndpoint: "https://cosmos:8081/",
+    authKeyOrResourceToken: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+);
+
+Database database = await client.CreateDatabaseIfNotExistsAsync(
+    id: "cosmicworks",
+    throughput: 400
+);
+
+Container container = await database.CreateContainerIfNotExistsAsync(
+    id: "products",
+    partitionKeyPath: "/id"
+);
+
+var item = new
+{
+    id = "68719518371",
+    name = "Kiama classic surfboard"
+};
+
+await container.UpsertItemAsync(item);
